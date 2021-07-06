d5825758ff9abc784b30722f0df6d08f57350661
Boyd Evans III <bbeiii@yahoo.com>
2021-07-06 09:41:24 -0600

    adding initial x axis setup

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,3 +18,8 @@ var svg = d3.select(".chart")
     .attr("width",svgWidth)
     .attr("height", svgHeight);
 
+var chartGroup = svg.append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+var initialXAxis = "poverty";
+
